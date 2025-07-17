99ed2502453f7a2ba206f25cf5a05e9f1a3a1a30
WillpowerfitnessAI <willpowerfitness@wheresyourwill.com>
2025-07-17 17:36:43 -0400

    Update subscribe.js

--- pages/subscribe.js
+++ pages/subscribe.js
@@ -1,25 +1,9 @@
-'use client';
-import React, { useEffect } from 'react';
+<script async
+  src="https://js.stripe.com/v3/buy-button.js">
+</script>
 
-export default function SubscribePage() {
-  useEffect(() => {
-    if (!window.customElements.get('stripe-buy-button')) {
-      const script = document.createElement('script');
-      script.src = 'https://js.stripe.com/v3/buy-button.js';
-      script.async = true;
-      document.body.appendChild(script);
-    }
-  }, []);
-
-  return (
-    <div style={{ padding: '4rem', textAlign: 'center' }}>
-      <h1>Join WillpowerFitness AI</h1>
-      <p>Train smarter. Think stronger. Move better.</p>
-
-      <stripe-buy-button
-        buy-button-id="buy_btn_1Rk2BwIW2TDvX8i08lvj2TXd"
-        publishable-key="pk_live_51RWp4dIw2TDvX8i03Cm21H03ZhWTmtkK0voD2mKiHfKIZDK7AdILcWQSDxLOv0BJzR1MLP00Kt1IW1PTf9MGGEAZS00y1xj6tAS">
-      </stripe-buy-button>
-    </div>
-  );
-}
+<stripe-buy-button
+  buy-button-id="buy_btn_1Rk2WBIw2TDvX8iO8lvj2TXd"
+  publishable-key="pk_live_51RWp4dIw2TDvX8iOJcm21OH32WTmtKkOvoD2mKiHfkIZDK7AdiLCWQSDxLoVOBjZR1MLP0OoKIlW1PTF9MGGEAZS00ylxj6tAS"
+>
+</stripe-buy-button>
