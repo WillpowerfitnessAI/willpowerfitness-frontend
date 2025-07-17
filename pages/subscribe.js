23e7f21c1b5a318434faac40c612fc4a4ca8f3ec
WillpowerfitnessAI <willpowerfitness@wheresyourwill.com>
2025-07-17 19:31:08 -0400

    Update subscribe.js

--- pages/subscribe.js
+++ pages/subscribe.js
@@ -1,9 +1,16 @@
-<script async
-  src="https://js.stripe.com/v3/buy-button.js">
-</script>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Test Stripe Button</title>
+    <script async src="https://js.stripe.com/v3/buy-button.js"></script>
+  </head>
+  <body>
+    <h1>Test Stripe Buy Button</h1>
 
-<stripe-buy-button
-  buy-button-id="buy_btn_1Rk2WBIw2TDvX8iO8lvj2TXd"
-  publishable-key="pk_live_51RWp4dIw2TDvX8iOJcm21OH32WTmtKkOvoD2mKiHfkIZDK7AdiLCWQSDxLoVOBjZR1MLP0OoKIlW1PTF9MGGEAZS00ylxj6tAS"
->
-</stripe-buy-button>
+    <stripe-buy-button
+      buy-button-id="buy_btn_1Rk2WBIw2TDvX8iO8lvj2TXd"
+      publishable-key="pk_live_51RWp4dIw2TDvX8iOJcm21OH32WTmtKkOvoD2mKiHfkIZDK7AdiLCWQSDxLoVOBjZR1MLP0OoKIlW1PTF9MGGEAZS00ylxj6tAS">
+    </stripe-buy-button>
+  </body>
+</html>
